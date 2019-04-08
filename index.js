f4d2ea45d0b1f762cf54116dab256a4a3d46372d
rekkyrek <hampus0505@gmail.com>
2019-04-08 22:15:03 +0200

    Added custom middlewares

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ import thunk from 'redux-thunk'
 import GenerateReducer from './GenerateReducer'
 import Connector from './Connector'
 
-export default (defaultState, hook) => {
+export default (defaultState, hook, customMiddlewares) => {
   const mappedReducers = {}
   Object.keys(defaultState).forEach(key => {
     mappedReducers[key] = GenerateReducer(key, defaultState[key], hook)
   })
 
   let reducers = combineReducers(mappedReducers)
+  let middlewares = [thunk, ...customMiddlewares]
   let store = createStore(reducers, compose(
-    applyMiddleware(thunk),
+    applyMiddleware(...middlewares),
     window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : a => a
   ))
   return store
